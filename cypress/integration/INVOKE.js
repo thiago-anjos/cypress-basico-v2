babc7b57c8e6bf9b78a86efc12632ae4f70d556a
thiago-anjos <your_email@example.com>
2022-09-03 21:38:57 -0300

    fix visit

--- cypress/integration/INVOKE.js
+++ cypress/integration/INVOKE.js
@@ -1,18 +1,39 @@
-it("exibe e esconde as mensagens de sucesso e erro usando o .invoke", () => {
-  cy.visitFilled();
+describe("usando invokes", () => {
+  beforeEach(() => {
+    cy.visitFilled();
+  });
 
-  cy.get(".success")
-    .should("not.be.visible")
-    .invoke("show")
-    .should("be.visible")
-    .and("contain", "Mensagem enviada com sucesso.")
-    .invoke("hide")
-    .should("not.be.visible");
-  cy.get(".error")
-    .should("not.be.visible")
-    .invoke("show")
-    .should("be.visible")
-    .and("contain", "Valide os campos obrigatórios!")
-    .invoke("hide")
-    .should("not.be.visible");
+  it("exibe e esconde as mensagens de sucesso e erro usando o .invoke", () => {
+    cy.visitFilled();
+
+    cy.get(".success")
+      .should("not.be.visible")
+      .invoke("show")
+      .should("be.visible")
+      .and("contain", "Mensagem enviada com sucesso.")
+      .invoke("hide")
+      .should("not.be.visible");
+    cy.get(".error")
+      .should("not.be.visible")
+      .invoke("show")
+      .should("be.visible")
+      .and("contain", "Valide os campos obrigatórios!")
+      .invoke("hide")
+      .should("not.be.visible");
+  });
+
+  it("Preenche a area de texto usando o invoke", () => {
+    const longText = Cypress._.repeat("0123456789", 20);
+
+    cy.get("#open-text-area")
+      .invoke("val", longText)
+      .should("have.value", longText);
+  });
+
+  it("Ache o gato", () => {
+    cy.get("#cat")
+      .should("not.be", "visible")
+      .invoke("show")
+      .should("be.visible");
+  });
 });
